8cf3fa81315a7bbc56fc4ca0db6f887dc591e173
JennyXJ <jenny860719jy@gmail.com>
2021-10-11 22:43:29 +0800

    購物車數字
    購物車數字

--- assets/home2.js
+++ assets/home2.js
@@ -1,7 +1,9 @@
 var num = 0;
+var shopNum = 0;
 
 function shopping(list,commodity,money,img) {
     num++;
+    shopNum++;
     document.getElementById("field").innerHTML += `<div class="btn-toolbar justify-content-between" role="toolbar" aria-label="Toolbar with button groups" id="ex${list}_${num}">
                                 <div class="btn-group" role="group" aria-label="First group">
                                     <img src="assets/img/${img}.png" alt="商品圖" class="img-thumbnail" style="height: 60px;">
@@ -14,9 +16,14 @@ function shopping(list,commodity,money,img) {
                                     <button class="btn btn-outline-danger" onclick="noShopping(${list},${num})">x</button>
                                 </div>
                             </div>`;
+    document.getElementById('shopNum').innerHTML =shopNum
+    if(shopNum>=1){document.getElementById('shopNum').style.display='inline'}
 
 }
 function noShopping(list,i) {
+    shopNum--;
+    document.getElementById('shopNum').innerHTML =shopNum;
+    if(shopNum==0){document.getElementById('shopNum').style.display='none'}
     document.getElementById(`ex${list}_${i}`).remove();
 }
 function count(list,money,i){
