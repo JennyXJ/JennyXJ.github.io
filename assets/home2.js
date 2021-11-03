183eb4d5779e4a6b8e8ea08381eeb0e414ddeea7
JennyXJ <jenny860719jy@gmail.com>
2021-11-04 02:13:14 +0800

    topnew
    topnew

--- assets/home2.js
+++ assets/home2.js
@@ -9,15 +9,14 @@ function shopping(list, commodity, money, img) {
                                 <div class="btn-group" role="group" aria-label="First group">
                                     <img src="assets/img/${img}.png" alt="商品圖" class="img-thumbnail" style="height: 60px;">
                                     <div class="align-self-center">${commodity}</div>
-                                    <div class="m-2"></div>
-                                    <div class="align-self-center" id="ex${list}Count${num}">NT$${money}</div>
+                                </div>
+                                <div class="input-group align-self-center">
+                                    <div class="align-self-center" style="color:coral;" id="ex${list}Count${num}">NT$${money}</div>
                                     <option value="${money}" name="minTotalMoney"></option>
+                                    <input style="width:45px;height:38px;" class="quantity" min="1" name="quantity" value="1" type="number" id="ex${list}Value${num}" onchange="count(${list},${money},${num})">
+                                    <button  class="btn text-danger" onclick="noShopping(${list},${num})">x</button>
                                 </div>
-                                <div class="input-group">
-                                    <input class="quantity" min="1" name="quantity" value="1" type="number" id="ex${list}Value${num}" onchange="count(${list},${money},${num})">
-                                    <button class="btn btn-outline-danger" onclick="noShopping(${list},${num})">x</button>
-                                </div>
-                            </div>`;
+                            </div><br>`;
   document.getElementById("shopNum").innerHTML = shopNum;
   if (shopNum >= 1) {
   document.getElementById("shopNum").style.display = "inline";
