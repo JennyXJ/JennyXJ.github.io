fc42a9f9322240dfb3df49af1726738ae6b46874
JennyXJ <jenny860719jy@gmail.com>
2021-12-05 13:55:06 +0800

    fade out
    fade out

--- assets/open.js
+++ assets/open.js
@@ -1,16 +1,19 @@
-setTimeout("changeState()", 30000);
+setTimeout("changeState()", 3000);
 
 function changeState() { document.getElementById('content').innerHTML = `<div class="fixed-top" id="xclose">
-        <div class="float-right card mr-2 mb-2 shadow-sm border-danger" style="width:15rem;">
-            <div class="modal-header">
-                <h5 class="modal-title">購買品項</h5>
-                <button type="button" class="btn btn-secondary" onclick="xCard()">x</button>
+        <div class="float-right card text-center shadow-sm border-danger" style="width:15rem;">
+            <div class="modal-header bg-danger text-white">
+                <h5 class="modal-title ">購買品項</h5>
+                <button type="button" class="btn btn-white" onclick="xCard()">x</button>
             </div>
             <div class="card-body">
-                <img src="assets/img/re.png" style="width:10rem;">
+                <img src="assets/img/re.png" class="mb-2" style="width:10rem;">
                 <h5 class="card-title">紅色口罩</h5>
                 <p class="card-text">請購買紅色口罩</p>
-                
+                <div class="card-footer text-muted" id="myMessage">
+                購買時間倒數
+                </div>
+                </div>
             </div>
         </div>
     </div>
@@ -20,3 +23,9 @@ function xCard() {
     console.log(123);
     document.getElementById('xclose').remove();
 }
+
+//setTimeout(function(){ 
+//    $('#xclose').remove(); 
+//}, 9000);
+
+
