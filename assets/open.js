38045092651e5e2be99f7fb940e1099a2395cfdb
JennyXJ <jenny860719jy@gmail.com>
2022-01-04 02:01:52 +0800

    over1
    over1

--- assets/open.js
+++ assets/open.js
@@ -1,5 +1,5 @@
-//紅色口罩
-setTimeout("changeState()", 3000);
+//+30Cheetos奇多-奇多蝦片原味
+setTimeout("changeState()", 30000);
 function changeState() { document.getElementById('content').innerHTML = `
 <div class="fixed-top" id="xclose">
         <div class="float-right card text-center shadow-sm border-danger" style="width:15rem;">
@@ -8,9 +8,9 @@ function changeState() { document.getElementById('content').innerHTML = `
                 <button type="button" class="btn"onclick="xCard()">x</button>
             </div>
             <div class="card-body">
-                <img src="assets/img/re.png" class="mb-2" style="width:10rem;">
-                <h5 class="card-title">紅色口罩</h5>
-                <p class="card-text">請購買紅色口罩</p>
+                <img src="assets/img/likecd.png" class="mb-2" style="width:10rem;">
+                <h5 class="card-title">Cheetos奇多-奇多蝦片原味(47g)</h5>
+                <p class="card-text">請購買 Cheetos奇多</p>
             </div>
             <div class="card-footer bg-danger text-white">
             15秒後關閉視窗
@@ -18,13 +18,13 @@ function changeState() { document.getElementById('content').innerHTML = `
         </div>
 </div> 
 `;}
-//紅色口罩x
+//+15Cheetos奇多-奇多蝦片原味x
 setTimeout(function(){ 
     $('#xclose').remove(); 
-}, 6000);
+}, 45000);
 
-//紫色口罩
-setTimeout("changeState2()", 9000);
+//+30韓國激安殿堂-竹鹽海苔
+setTimeout("changeState2()", 75000);
 function changeState2() { document.getElementById('content').innerHTML = `
 <div class="fixed-top" id="xclose">
         <div class="float-right card text-center shadow-sm border-danger" style="width:15rem;">
@@ -33,9 +33,9 @@ function changeState2() { document.getElementById('content').innerHTML = `
                 <button type="button" class="btn"onclick="xCard()">x</button>
             </div>
             <div class="card-body">
-                <img src="assets/img/ne.png" class="mb-2" style="width:10rem;">
-                <h5 class="card-title">紫色口罩</h5>
-                <p class="card-text">請購買紫色口罩</p>
+                <img src="assets/img/likelcht.png" class="mb-2" style="width:10rem;">
+                <h5 class="card-title">韓國-激安殿堂竹鹽海苔</h5>
+                <p class="card-text">請購買 激安殿堂竹鹽海苔</p>
             </div>
             <div class="card-footer bg-danger text-white">
             15秒後關閉視窗
@@ -43,13 +43,156 @@ function changeState2() { document.getElementById('content').innerHTML = `
         </div>
 </div> 
 `;}
-//紫色口罩x
+//+15韓國激安殿堂-竹鹽海苔x
 setTimeout(function(){ 
     $('#xclose').remove(); 
-}, 12000);
+}, 90000);
+
+//+30統一-麥香紅茶300ml
+setTimeout("changeState3()", 120000);
+function changeState3() { document.getElementById('content').innerHTML = `
+<div class="fixed-top" id="xclose">
+        <div class="float-right card text-center shadow-sm border-danger" style="width:15rem;">
+            <div class="modal-header">
+                <h5 class="modal-title ">購買品項</h5>
+                <button type="button" class="btn"onclick="xCard()">x</button>
+            </div>
+            <div class="card-body">
+                <img src="assets/img/likemc.png" class="mb-2" style="width:10rem;">
+                <h5 class="card-title">統一-麥香紅茶300ml</h5>
+                <p class="card-text">請購買 麥香紅茶300ml</p>
+            </div>
+            <div class="card-footer bg-danger text-white">
+            15秒後關閉視窗
+            </div>
+        </div>
+</div> 
+`;}
+//+15統一-麥香紅茶300mlx
+setTimeout(function(){ 
+    $('#xclose').remove(); 
+}, 135000);
+
+//+30光泉-全脂牛乳200ml
+setTimeout("changeState4()", 165000);
+function changeState4() { document.getElementById('content').innerHTML = `
+<div class="fixed-top" id="xclose">
+        <div class="float-right card text-center shadow-sm border-danger" style="width:15rem;">
+            <div class="modal-header">
+                <h5 class="modal-title ">購買品項</h5>
+                <button type="button" class="btn"onclick="xCard()">x</button>
+            </div>
+            <div class="card-body">
+                <img src="assets/img/likekc.png" class="mb-2" style="width:10rem;">
+                <h5 class="card-title">光泉-全脂牛乳200ml</h5>
+                <p class="card-text">請購買 光泉-全脂牛乳</p>
+            </div>
+            <div class="card-footer bg-danger text-white">
+            15秒後關閉視窗
+            </div>
+        </div>
+</div> 
+`;}
+//+15光泉-全脂牛乳200mlx
+setTimeout(function(){ 
+    $('#xclose').remove(); 
+}, 180000);
+
+//+30樂事-美國經典原味洋芋片
+setTimeout("changeState5()", 210000);
+function changeState5() { document.getElementById('content').innerHTML = `
+<div class="fixed-top" id="xclose">
+        <div class="float-right card text-center shadow-sm border-danger" style="width:15rem;">
+            <div class="modal-header">
+                <h5 class="modal-title ">購買品項</h5>
+                <button type="button" class="btn"onclick="xCard()">x</button>
+            </div>
+            <div class="card-body">
+                <img src="assets/img/likelc.png" class="mb-2" style="width:10rem;">
+                <h5 class="card-title">樂事-美國經典原味洋芋片</h5>
+                <p class="card-text">請購買 樂事-美國經典原味洋芋片</p>
+            </div>
+            <div class="card-footer bg-danger text-white">
+            15秒後關閉視窗
+            </div>
+        </div>
+</div> 
+`;}
+//+15樂事-美國經典原味洋芋片x
+setTimeout(function(){ 
+    $('#xclose').remove(); 
+}, 225000);
+
+//+30義美-牛奶小泡芙
+setTimeout("changeState6()", 255000);
+function changeState6() { document.getElementById('content').innerHTML = `
+<div class="fixed-top" id="xclose">
+        <div class="float-right card text-center shadow-sm border-danger" style="width:15rem;">
+            <div class="modal-header">
+                <h5 class="modal-title ">購買品項</h5>
+                <button type="button" class="btn"onclick="xCard()">x</button>
+            </div>
+            <div class="card-body">
+                <img src="assets/img/likelcem.png" class="mb-2" style="width:10rem;">
+                <h5 class="card-title">義美-牛奶小泡芙</h5>
+                <p class="card-text">請購買 義美-牛奶小泡芙</p>
+            </div>
+            <div class="card-footer bg-danger text-white">
+            15秒後關閉視窗
+            </div>
+        </div>
+</div> 
+`;}
+//+15義美-牛奶小泡芙x
+setTimeout(function(){ 
+    $('#xclose').remove(); 
+}, 270000);
+
+//+30每朝健康-綠茶650ml
+setTimeout("changeState7()", 300000);
+function changeState7() { document.getElementById('content').innerHTML = `
+<div class="fixed-top" id="xclose">
+        <div class="float-right card text-center shadow-sm border-danger" style="width:15rem;">
+            <div class="modal-header">
+                <h5 class="modal-title ">購買品項</h5>
+                <button type="button" class="btn"onclick="xCard()">x</button>
+            </div>
+            <div class="card-body">
+                <img src="assets/img/likemg.png" class="mb-2" style="width:10rem;">
+                <h5 class="card-title">每朝健康-綠茶650ml</h5>
+                <p class="card-text">請購買 每朝健康-綠茶650ml</p>
+            </div>
+            <div class="card-footer bg-danger text-white">
+            15秒後關閉視窗
+            </div>
+        </div>
+</div> 
+`;}
+//+15每朝健康-綠茶650mlx
+setTimeout(function(){ 
+    $('#xclose').remove(); 
+}, 315000);
+
+//+2分45謝謝您的購買
+setTimeout("changeState8()", 480000);
+function changeState8() { document.getElementById('content').innerHTML = `
+
+        <div class="modal-dialog modal-dialog-centered bg-dark" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                </div>
+                <div class="modal-body text-center">
+                    <h1 class="text-danger">THANK YOU</h1><br>感謝您的購買:)
+                </div>
+
+            </div>
+        </div>
+`;}
 
 //打叉關閉
 function xCard() {
     console.log(123);
     document.getElementById('xclose').remove();
 }
+
+
